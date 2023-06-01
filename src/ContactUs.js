d3aacf36e79f6efd367ef0dfad4f8612d7672afb
dedeVilius <vilius@godigi.lt>
2023-06-01 09:14:33 +0300

    Second commit

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -40,7 +40,7 @@ function ContactUs() {
           </div>
         </div>
         <div className="right-side">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2294.3423087089504!2d23.8857853165754!3d54.89691388033314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7220ff3184881%3A0x8002872ac9bbdb7c!2sRotu%C5%A1%C4%97s%20a.%201%2C%2044280%20Kaunas!5e0!3m2!1slt!2slt!4v1680176949803!5m2!1slt!2slt" width={1000} height={600} style={{ border : 0 }} title='Google Maps' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2294.3423087089504!2d23.8857853165754!3d54.89691388033314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7220ff3184881%3A0x8002872ac9bbdb7c!2sRotu%C5%A1%C4%97s%20a.%201%2C%2044280%20Kaunas!5e0!3m2!1slt!2slt!4v1680176949803!5m2!1slt!2slt" width={1000} height={600} style={{ border : 0 }} title='Google' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>

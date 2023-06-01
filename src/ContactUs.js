29cd409403fe4300e7ceb062eb7f870b3da72257
dedeVilius <vilius@godigi.lt>
2023-06-01 09:13:02 +0300

    First commit

---
+++ src/ContactUs.js
@@ -0,0 +1,50 @@
+function ContactUs() {
+  return (
+    <div className="content-wrapper">
+      <div className="container">
+        <div className="left-side">
+          <h1>Find Us</h1>
+          <div className="wrapper">
+            <div className="contact-box">
+              <h2>Parduotuvė 1</h2>
+              <ul>
+                <li><span>Phone:</span> <a href="http://">+37066666666</a></li>
+                <li><span>Email:</span> <a href="http://">shop1@parduotuve.lt</a></li>
+                <li><span>Address:</span><a href="http://"> Rotušės aikštė 1, Kaunas</a></li>
+              </ul>
+            </div>
+            <div className="contact-box">
+              <h2>Parduotuvė 2</h2>
+              <ul>
+                <li><span>Phone:</span> <a href="http://">+37066666666</a></li>
+                <li><span>Email:</span> <a href="http://">shop1@parduotuve.lt</a></li>
+                <li><span>Address:</span><a href="http://"> Rotušės aikštė 1, Kaunas</a></li>
+              </ul>
+            </div>
+            <div className="contact-box">
+              <h2>Parduotuvė 3</h2>
+              <ul>
+                <li><span>Phone:</span> <a href="http://">+37066666666</a></li>
+                <li><span>Email:</span> <a href="http://">shop1@parduotuve.lt</a></li>
+                <li><span>Address:</span><a href="http://"> Rotušės aikštė 1, Kaunas</a></li>
+              </ul>
+            </div>
+            <div className="contact-box">
+              <h2>Parduotuvė 4</h2>
+              <ul>
+                <li><span>Phone:</span> <a href="http://">+37066666666</a></li>
+                <li><span>Email:</span> <a href="http://">shop1@parduotuve.lt</a></li>
+                <li><span>Address:</span><a href="http://"> Rotušės aikštė 1, Kaunas</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="right-side">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2294.3423087089504!2d23.8857853165754!3d54.89691388033314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7220ff3184881%3A0x8002872ac9bbdb7c!2sRotu%C5%A1%C4%97s%20a.%201%2C%2044280%20Kaunas!5e0!3m2!1slt!2slt!4v1680176949803!5m2!1slt!2slt" width={1000} height={600} style={{ border : 0 }} title='Google Maps' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactUs;
